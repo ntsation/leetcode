b28ca1e01444c52d0c7b7301b9a853d00698defd
Nathan Souza <naathanmarinho11@gmail.com>
2024-03-16 21:23:37 -0300

    add case 2

---
+++ python/2_add_two_num.py
@@ -0,0 +1,29 @@
+class ListNode:
+   def __init__(self, val=0, next=None):
+      self.val = val
+      self.next = next
+
+class Solution:
+   def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+      dummy_head = ListNode(0)
+      current = dummy_head
+      carry = 0
+
+      while l1 or l2:
+         val1 = l1.val if l1 else 0
+         val2 = l2.val if l2 else 0
+
+         total = val1 + val2 + carry
+         carry = total // 10
+         digit = total % 10
+
+         current.next = ListNode(digit)
+         current = current.next
+
+         if l1:
+            l1 = l1.next
+         if l2:
+            l2 = l2.next
+      if carry > 0:
+         current.next = ListNode(carry)
+      return dummy_head.next
